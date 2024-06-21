68e05af6adafdd0575db0f0d461361b2260315cf
jone-pecca <jonejunior2231@hotmail.com>
2024-06-21 16:02:31 -0300

    first commit, start the project

---
+++ routes/users.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+
+// Get all users
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Get one user
+router.get('/:id', getUser, (req, res) => {
+  res.json(res.user);
+});
+
+// Create or update a user (Upsert)
+router.post('/', async (req, res) => {
+  const { name, email } = req.body;
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { email: email }, // Criteria to find the document
+      { name: name, email: email }, // Data to update
+      { new: true, upsert: true } // Options: return the updated doc and upsert if not found
+    );
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Update a user
+router.put('/:id', getUser, async (req, res) => {
+  if (req.body.name != null) {
+    res.user.name = req.body.name;
+  }
+  if (req.body.email != null) {
+    res.user.email = req.body.email;
+  }
+
+  try {
+    const updatedUser = await res.user.save();
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete a user
+router.delete('/:id', getUser, async (req, res) => {
+  try {
+    await res.user.deleteOne(); // Using deleteOne instead of remove
+    res.json({ message: 'Deleted User' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Middleware to get user object by ID
+async function getUser(req, res, next) {
+  let user;
+  try {
+    user = await User.findById(req.params.id);
+    if (user == null) {
+      return res.status(404).json({ message: 'Cannot find user' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.user = user;
+  next();
+}
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js (update with user routes)
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://mongo:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Routes
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
